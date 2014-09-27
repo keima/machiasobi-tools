b0cb1b4425849b1a0156d5c3e5688b2ec9c2d1dc
Kouta Imanaka <keima@p-side.net>
2014-09-27 23:39:23 +0900

    GETリクエストだけ Access-Control-Allow-Headers: * をセットするようにした

--- machitools/middleware.go
+++ machitools/middleware.go
@@ -15,72 +15,20 @@ func (MyMiddleware) MiddlewareFunc(handler rest.HandlerFunc) rest.HandlerFunc {
 		// Be nice with non CORS requests, continue
 		// Alternatively, you may also chose to only allow CORS requests, and return an error.
 		if !corsInfo.IsCors {
-			// continure, execute the wrapped middleware
+			// continue, execute the wrapped middleware
 			handler(writer, request)
 			return
 		}
 
-		// Validate the Origin
-
-		allowList := []interface{}{
-			"http://machi.p-side.net",
-			"http://localhost:3000",
-		}
-
-		if  !Contains(allowList, corsInfo.Origin) {
+		if request.Method == "GET" {
+			writer.Header().Set("Access-Control-Allow-Origin", "*")
+			handler(writer, request)
+			return
+		} else {
+			// Probably, error string will hidden by browser.
 			rest.Error(writer, "Invalid Origin", http.StatusForbidden)
 			return
 		}
 
-		if corsInfo.IsPreflight {
-			// check the request methods
-			allowedMethods := map[string]bool{
-				"GET":  true,
-				"POST": true,
-				"PUT":  true,
-				// don't allow DELETE, for instance
-			}
-			if !allowedMethods[corsInfo.AccessControlRequestMethod] {
-				rest.Error(writer, "Invalid Preflight Request", http.StatusForbidden)
-				return
-			}
-			// check the request headers
-			allowedHeaders := map[string]bool{
-				"Accept":          true,
-				"Content-Type":    true,
-				"X-Custom-Header": true,
-			}
-			for _, requestedHeader := range corsInfo.AccessControlRequestHeaders {
-				if !allowedHeaders[requestedHeader] {
-					rest.Error(writer, "Invalid Preflight Request", http.StatusForbidden)
-					return
-				}
-			}
-
-			for allowedMethod, _ := range allowedMethods {
-				writer.Header().Add("Access-Control-Allow-Methods", allowedMethod)
-			}
-			for allowedHeader, _ := range allowedHeaders {
-				writer.Header().Add("Access-Control-Allow-Headers", allowedHeader)
-			}
-			writer.Header().Set("Access-Control-Allow-Origin", corsInfo.Origin)
-			writer.Header().Set("Access-Control-Allow-Credentials", "true")
-			writer.Header().Set("Access-Control-Max-Age", "3600")
-			writer.WriteHeader(http.StatusOK)
-			return
-		} else {
-			writer.Header().Set("Access-Control-Expose-Headers", "X-Powered-By")
-			writer.Header().Set("Access-Control-Allow-Origin", corsInfo.Origin)
-			writer.Header().Set("Access-Control-Allow-Credentials", "true")
-			// continure, execute the wrapped middleware
-			handler(writer, request)
-			return
-		}
-
 	}
-}
-
-func Contains(list []interface{}, elem interface{}) bool {
-	for _, t := range list { if t == elem { return true } }
-	return false
 }
