d6c790b1e9ccd758169f2647950f034ff43305c1
Kouta Imanaka <keima@p-side.net>
2015-03-27 12:33:47 +0900

    oops x2

--- script/ci/after_success.sh
+++ script/ci/after_success.sh
@@ -4,6 +4,7 @@
 export GAE_DIR=$GO_APPENGINE_PATH
 export APP_DIR=$TRAVIS_BUILD_DIR/machitools
 
-echo "PR# $TRAVIS_PULL_REQUEST"
 cd $APP_DIR
-python $GAE_DIR/appcfg.py --oauth2_refresh_token=$GAE_OAUTH update app-prod.yaml
+rm app.yaml
+cp app-prod.yaml app.yaml
+python $GAE_DIR/appcfg.py --oauth2_refresh_token=$GAE_OAUTH update ./
