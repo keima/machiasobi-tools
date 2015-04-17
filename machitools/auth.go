cf3d2239a3f4a31d605764b5368d7d8f2c4d0686
Kouta Imanaka <keima@p-side.net>
2015-04-17 16:32:52 +0900

    /calendar とそれに関する認証系の作成、通信部分の見直し

--- machitools/auth.go
+++ machitools/auth.go
@@ -3,11 +3,20 @@ package machitools
 import (
 	"net/http"
 
+	"github.com/keima/machitools/customer"
+
 	"gopkg.in/ant0ine/go-json-rest.v2/rest"
 
 	"appengine"
+	"appengine/datastore"
 	"appengine/user"
 )
+
+const (
+	machiAppURL = "http://machi.p-side.net/"
+)
+
+// TODO: 要リファクタリング
 
 func Login(w rest.ResponseWriter, r *rest.Request) {
 	c := appengine.NewContext(r.Request)
@@ -22,9 +31,14 @@ func Login(w rest.ResponseWriter, r *rest.Request) {
 		w.Header().Set("Location", url)
 		w.WriteHeader(http.StatusFound)
 	} else {
+		if err := createUserIfNeed(c, u); err != nil {
+			rest.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
 		redirect := "/"
 		if r.FormValue("redirectTo") == "app" {
-			redirect = "http://machi.p-side.net/"
+			redirect = machiAppURL
 		}
 
 		w.Header().Set("Location", redirect)
@@ -47,7 +61,7 @@ func Logout(w rest.ResponseWriter, r *rest.Request) {
 	} else {
 		redirect := "/"
 		if r.FormValue("redirectTo") == "app" {
-			redirect = "http://machi.p-side.net/"
+			redirect = machiAppURL
 		}
 
 		w.Header().Set("Location", redirect)
@@ -66,4 +80,19 @@ func CheckStatus(w rest.ResponseWriter, r *rest.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.WriteJson(u)
 	}
+}
+
+func createUserIfNeed(c appengine.Context, u *user.User) error {
+	customer := customer.CustomerItem{}
+
+	customer.Init(u)
+	if err := customer.Load(c); err != nil {
+		if err.Error() == datastore.ErrNoSuchEntity.Error() {
+			c.Infof("User created: %s", customer.ID)
+			customer.Save(c)
+		} else {
+			return err
+		}
+	}
+	return nil
 }
